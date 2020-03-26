40febb19c53d6c5018a82f898a49d15ec322343a
vinicius-maruchi <maruchi.vinicius@gmail.com>
2020-03-26 20:18:32 -0300

    database

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-   const body = request.body;
-   
-   return response.json({
-    evento: 'Semana OmniStack 11',
-    aluno: 'Cleiton Souza'
+routes.post('/ongs', async (request, response) => {
+   const { name, email, whatsapp, city, uf } = request.body;
+
+   const id = crypto.randomBytes(4).toString('HEX');
+
+   await connection('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
    });
+   
+   return response.json({ id });
 });
 
 module.exports = routes;
